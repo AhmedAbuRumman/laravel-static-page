97f4f5516c28d5c17d7ea2bbdc766ba3c1a04aed
AhmedAbuRumman <Ahmed.e.aburuman@gamil.com>
2020-12-27 02:07:25 +0200

    commit

---
+++ blog/app/Http/Controllers/controllerName.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controllerName extends Controller
+{
+   
+
+
+public function home(){
+
+    return view('home');
+}
+
+public function contactus(){
+
+    return view('contact-us');
+}
+
+}
